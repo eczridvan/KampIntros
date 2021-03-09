2f10674e9a082dae1dc33ad39a88ced078fb7814
RIDVAN BOZKIR <ridvanbozkir@yandex.com>
2021-03-09 07:51:58 +0300

    09.03.2020 tekrar

---
+++ Faktoriyel/FaktorBul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faktoriyel
+{
+    public class FaktorBul
+    {
+        public int OzyinelemeliFaktoriyel(int sayi)
+        {
+            if (sayi == 1)
+                return sayi;
+            else
+                return sayi * OzyinelemeliFaktoriyel(sayi - 1);
+        }
+    }
+}
---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Faktoriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            FaktorBul faktorBul = new FaktorBul();
+
+            int result = faktorBul.OzyinelemeliFaktoriyel(6);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
